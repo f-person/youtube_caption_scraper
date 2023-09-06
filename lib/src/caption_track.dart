f3ce3a9ac3d22a7c7a718a130cf843150739b28d
DdavidC <ddavid.chung@gmail.com>
2023-09-06 16:15:23 +0800

    feat: add property kind into CaptionTrack

--- lib/src/caption_track.dart
+++ lib/src/caption_track.dart
@@ -4,6 +4,7 @@ class CaptionTrack {
     required this.baseUrl,
     required this.name,
     required this.languageCode,
+    required this.kind,
   });
 
   /// Base URL of the caption track. This is used to get actual subtitles.
@@ -15,6 +16,9 @@ class CaptionTrack {
   /// The language code of the caption track.
   final String languageCode;
 
+  /// The kind of the caption track. The kind of a caption track generated using automatic speech recognition will be 'asr'.
+  final String kind;
+
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
@@ -22,13 +26,14 @@ class CaptionTrack {
     return other is CaptionTrack &&
         other.baseUrl == baseUrl &&
         other.name == name &&
-        other.languageCode == languageCode;
+        other.languageCode == languageCode &&
+        other.kind == kind;
   }
 
   @override
-  int get hashCode => baseUrl.hashCode ^ name.hashCode ^ languageCode.hashCode;
+  int get hashCode => baseUrl.hashCode ^ name.hashCode ^ languageCode.hashCode ^ kind.hashCode;
 
   @override
   String toString() =>
-      'CaptionTrack(baseUrl: $baseUrl, name: $name, languageCode: $languageCode)';
+      'CaptionTrack(baseUrl: $baseUrl, name: $name, languageCode: $languageCode, kind: $kind)';
 }
